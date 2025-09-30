c8c0a97a460e58a047c3f4c7a69791695b4f6af3
rksorry7255tu <rk7255053708@gmail.com>
2025-09-30 13:06:48 +0530

    Currency converter

---
+++ frontend5/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl bg-orange-500">Currency app with chai.</h1>
+    </>
+  );
+}
+
+export default App;
