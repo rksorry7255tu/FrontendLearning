49526672e9cae3887d144938020fe00d855568d9
rksorry7255tu <rk7255053708@gmail.com>
2025-09-30 16:26:20 +0530

    commited

--- frontend5/src/App.jsx
+++ frontend5/src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 import { useState } from "react";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
+  const data = useCurrencyInfo("usd");
+  console.log(data);
+
   return (
     <>
       <h1 className="text-3xl bg-orange-500">Currency app with chai.</h1>
